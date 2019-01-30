79be83e769b92b3488962a7313876b9ffcd868ee
XiaoyongXu <xxy1994212@gmail.com>
2019-01-30 01:25:06 +0000

    update migrate

---
+++ migrations/20190130010425_addForeign_key.js
@@ -0,0 +1,31 @@
+// exports.up = function(knex, Promise) {  
+//   return Promise.all([
+//     knex.schema.table('users', function(table){
+//       table.string('twitter');
+//     })
+//   ])
+// };
+
+// exports.down = function(knex, Promise) {  
+//   return Promise.all([
+//     knex.schema.table('users', function(table){
+//       table.dropColumn('twitter');
+//     })
+//   ])
+// };
+
+exports.up = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.integer('famous_person_id').references('famous_people.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.dropColumn('famous_person_id');
+    })
+  ])
+};
